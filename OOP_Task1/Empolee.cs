141968869e2d42cad07d08d8844ab87c2089b72c
Sahib Novruzov <sahib.novruzov33@hotmail.com>
2023-06-06 13:15:48 +0400

    Add project files.

---
+++ OOP_Task1/Empolee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Task1
+{
+    public class Employee
+    {
+        short id;
+        string name;
+        string surname;
+        byte age;
+        string workplace;
+        string position;
+        short salary;
+        string country;
+        public short Id { get { return id; } set { id = value; } }
+        public string Name { get { return name; } set { name = value; } }
+        public string Surname { get { return surname; } set { surname = value; } }
+        public byte Age { get { return age; } set { age = value; } }
+        public string Workplace { get { return workplace; } set { workplace = value; } }
+        public string Position { get { return position; } set { position = value; } }
+        public short Salary { get { return salary; } set { salary = value; } }
+        public string Country { get { return "England"; } }
+        public void GetInfo1()
+        {
+            Console.WriteLine($"{this.Id = 2}\n{this.Name ="John"}\n{this.Surname ="Carter"}\n{this.Age =26}\n{this.Workplace ="IT software engineering school"}\n{this.Position ="IT teacher"}\n{this.Salary =2000}\n{this.Country}\n");
+        }
+        public void GetInfo2()
+        {
+            Console.WriteLine($"{this.Id = 3}\n{this.Name = "Alex"}\n{this.Surname = "Tampsey"}\n{this.Age = 28}\n{this.Workplace = "Data School"}\n{this.Position = "Data engineer"}\n{this.Salary = 1200}\n{this.Country}");
+        }
+
+    }
+}
---
+++ OOP_Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using OOP_Task1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Employee employee = new Employee();
+
+        employee.Id = 1;
+        employee.Name = "Max";
+        employee.Surname = "Lindegaard";
+        employee.Age = 30;
+        employee.Position = "IT Support";
+        employee.Workplace = "Technology ";
+        employee.Salary = 1500;
+
+        Console.WriteLine($"{employee.Id}\n{employee.Name}\n{employee.Surname}\n{employee.Age}\n{employee.Workplace}\n{employee.Position}\n{employee.Salary}\n{employee.Country}\n");
+        employee.GetInfo1();
+        employee.GetInfo2();
+
+        Console.ReadLine();
+    }
+}
